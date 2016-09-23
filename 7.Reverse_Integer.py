e76ba860760af0d0a2cf135f2426b2d024221ee8
Mike <mike@MikedeiMac.Home>
2016-09-23 23:25:53 +0800

    refine 7.Reverse_Integer.py

--- 7.Reverse_Integer.py
+++ 7.Reverse_Integer.py
@@ -8,12 +8,10 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < 0:
-            n = -int(str(x)[1:][::-1])
-            return n if n >= -math.pow(2, 31) else 0
-        else:
-            n = int(str(x)[::-1])
-            return n if n < math.pow(2, 31) else 0
+        n = int(str(abs(x))[::-1])
+        if n > 0x7fffffff:
+            return 0
+        return n if x >= 0 else -n
 
 for i in xrange(-100, 100):
     print([i, Solution().reverse(i)])
