cd0664faebc68622b6f7dc84ea55c6cf152feaae
Mike <mike@MikedeiMac.Home>
2016-09-23 23:34:37 +0800

    refine 7.Reverse_Integer.py

--- 7.Reverse_Integer.py
+++ 7.Reverse_Integer.py
@@ -8,10 +8,18 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        n = int(str(abs(x))[::-1])
+        n, y = 0, abs(x)
+        while y > 0:
+            d, m = divmod(y, 10)
+            n, y = n*10+m, d
+
         if n > 0x7fffffff:
             return 0
         return n if x >= 0 else -n
 
 for i in xrange(-100, 100):
     print([i, Solution().reverse(i)])
+i = 123456
+print([i, Solution().reverse(i)])
+i = -123456
+print([i, Solution().reverse(i)])
